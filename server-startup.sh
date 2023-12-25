a63eef0a0166fae65a902549f2ef80656d4ec476
sahil kataria <sahilkataria.1989@gmail.com>
2023-12-25 13:00:35 +0530

    server startup

--- server-startup.sh
+++ server-startup.sh
@@ -6,5 +6,4 @@ apt-get upgrade -y
 apt-get install docker-compose docker.io unzip -y
 
 export $(cat .env | xargs)
-#echo $dockerpass |docker login -u prabinjha --password-stdin
 docker-compose up -d
